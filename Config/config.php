0cbd5330ad25a4acdab3843dc60de4b31dbc2526
JonnyBo <526@talp.ru>
2021-06-12 18:56:50 +0300

    начальный вариант

---
+++ Config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'name' => 'Chat',
+    'server' => 'https://m.celeviki.com/',
+    'reg_url' => 'https://m.celeviki.com/api/user.php',
+    'auth_url' => 'https://m.celeviki.com/api/user.php'
+
+
+];
